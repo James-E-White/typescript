5e0d780327830745858f1a52f4e59648c4ab8222
James White <whitejaad@gmail.com>
2025-02-21 15:27:11 -0800

    added grade

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
-let message = 'Hello!';
-console.log(message);
+let student = "John";
+let grade1 = parseFloat("8");
+let grade2 = parseFloat("9");
+let grade3 = parseFloat("7");
+let isApproved;
+function aver(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+isApproved = aver(grade1, grade2, grade3) > 7;
+console.log(aver(grade1, grade2, grade3));
+console.log(`The student ${student} is approved? ${isApproved}`);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-let message: string = 'Hello!';
-console.log(message);
+let student : string = "John";
+let grade1 = parseFloat("8");
+let grade2 = parseFloat("9");
+let grade3 = parseFloat("7");
+let isApproved : boolean;
+
+function aver(num1:number, num2:number, num3:number) : number{
+    return (num1 + num2 + num3) / 3;
+}
+
+isApproved = aver(grade1, grade2, grade3) > 7;
+
+console.log( aver(grade1, grade2, grade3) );
+console.log( `The student ${student} is approved? ${isApproved}` );
+
+
+
